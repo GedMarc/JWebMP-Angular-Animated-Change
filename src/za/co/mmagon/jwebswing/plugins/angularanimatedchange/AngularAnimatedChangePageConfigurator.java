3001dc1c5592bc760047c754906f1b9cfd9fc05d
Marc Magon <ged_marc@hotmail.com>
2017-11-30 20:39:22 +0200

    Pom file cleanup, maven dependency plugin removed????

--- src/za/co/mmagon/jwebswing/plugins/angularanimatedchange/AngularAnimatedChangePageConfigurator.java
+++ src/za/co/mmagon/jwebswing/plugins/angularanimatedchange/AngularAnimatedChangePageConfigurator.java
@@ -8,12 +8,7 @@ import za.co.mmagon.jwebswing.plugins.ComponentInformation;
 import za.co.mmagon.jwebswing.plugins.PluginInformation;
 import za.co.mmagon.jwebswing.plugins.jquery.JQueryPageConfigurator;
 
-/**
- * Allows for elements to have a zoom in animation by adding a class
- *
- * @author Marc Magon
- * @since 16 Jun 2017
- */
+
 @PluginInformation(pluginName = "Angular Animated Change",
 		pluginDescription = "Changes values with an animation",
 		pluginUniqueName = "jwebswing-angular-animated-change",
@@ -31,6 +26,12 @@ import za.co.mmagon.jwebswing.plugins.jquery.JQueryPageConfigurator;
 @ComponentInformation(name = "Angular Animated Change",
 		description = "Apply an animation to changing values with this angular directive",
 		url = "https://www.akveo.com/products.html")
+/**
+ * Allows for elements to have a zoom in animation by adding a class
+ *
+ * @author Marc Magon
+ * @since 16 Jun 2017
+ */
 public class AngularAnimatedChangePageConfigurator extends PageConfigurator
 {
 
@@ -44,6 +45,11 @@ public class AngularAnimatedChangePageConfigurator extends PageConfigurator
 		//Nothing needed
 	}
 
+	/**
+	 * Adds the zoom effect to the component
+	 *
+	 * @param component
+	 */
 	public static void addZoomEffect(ComponentHierarchyBase component)
 	{
 		component.addClass("zoomIn");
