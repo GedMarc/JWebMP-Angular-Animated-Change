68c3554262a3f6edcdf62cc711f2af5416830287
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:39:43 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,14 +1,21 @@
 import com.jwebmp.core.base.angular.services.IAngularDirective;
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.plugins.angularanimatedchange.AngularAnimatedChangeDirective;
 import com.jwebmp.plugins.angularanimatedchange.AngularAnimatedChangePageConfigurator;
+import com.jwebmp.plugins.angularanimatedchange.implementations.AngularAnimatedChangeExclusionsModule;
 
 module com.jwebmp.plugins.angularanimatedchange {
 	exports com.jwebmp.plugins.angularanimatedchange;
 
 	requires com.jwebmp.core;
 	requires java.validation;
+	requires com.jwebmp.guicedinjection;
 
 	provides IPageConfigurator with AngularAnimatedChangePageConfigurator;
 	provides IAngularDirective with AngularAnimatedChangeDirective;
+
+	provides IGuiceScanJarExclusions with AngularAnimatedChangeExclusionsModule;
+	provides IGuiceScanModuleExclusions with AngularAnimatedChangeExclusionsModule;
 }
---
+++ src/main/java/com/jwebmp/plugins/angularanimatedchange/implementations/AngularAnimatedChangeExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.angularanimatedchange.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AngularAnimatedChangeExclusionsModule
+		implements IGuiceScanModuleExclusions<AngularAnimatedChangeExclusionsModule>,
+				           IGuiceScanJarExclusions<AngularAnimatedChangeExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-angular-animated-change-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.angularanimatedchange");
+		return strings;
+	}
+}
