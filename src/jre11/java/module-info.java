43f4864eb54676b5d12885f05b7a5606b1667792
GedMarc <ged_marc@hotmail.com>
2019-10-26 02:34:47 +0200

    Guiced EE Migration to its own space

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -3,12 +3,12 @@ module com.jwebmp.plugins.angularanimatedchange {
 
 	requires com.jwebmp.core;
 	requires java.validation;
-	requires com.jwebmp.guicedinjection;
+	requires com.guicedee.guicedinjection;
 	requires com.jwebmp.core.angularjs;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.angularanimatedchange.AngularAnimatedChangePageConfigurator;
 	provides com.jwebmp.core.base.angular.services.IAngularDirective with com.jwebmp.plugins.angularanimatedchange.AngularAnimatedChangeDirective;
 
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.angularanimatedchange.implementations.AngularAnimatedChangeExclusionsModule;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.angularanimatedchange.implementations.AngularAnimatedChangeExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.angularanimatedchange.implementations.AngularAnimatedChangeExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.angularanimatedchange.implementations.AngularAnimatedChangeExclusionsModule;
 }
