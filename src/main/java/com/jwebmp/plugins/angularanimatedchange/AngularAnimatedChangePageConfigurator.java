a46ad9908be7321920642751e1944a08affd11eb
GedMarc <ged_marc@hotmail.com>
2020-11-12 01:18:43 +0200

    Jakarta Namespace - JLink building

--- src/main/java/com/jwebmp/plugins/angularanimatedchange/AngularAnimatedChangePageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/angularanimatedchange/AngularAnimatedChangePageConfigurator.java
@@ -25,7 +25,7 @@ import com.jwebmp.core.plugins.PluginInformation;
 import com.jwebmp.core.plugins.jquery.JQueryPageConfigurator;
 import com.jwebmp.core.services.IPageConfigurator;
 
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 @PluginInformation(pluginName = "Angular Animated Change",
 		pluginDescription = "Changes values with an animation",
